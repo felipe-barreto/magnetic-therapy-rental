e159d430b4f0e34b594fcc2cb573b637dd39d765
Felipe Barreto <felipebarreto00000@gmail.com>
2025-07-19 19:58:03 -0300

    feat: create home view

---
+++ pages/Home/Home.tsx
@@ -0,0 +1,45 @@
+import styles from "./Home.module.css";
+import imageForLargeScreens from "../../src/assets/home-for-large-screens.png";
+import imageForSmallScreens from "../../src/assets/home-for-small-screens.png";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowWidthChange = () => {
+      setInnerWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowWidthChange);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowWidthChange);
+    };
+  }, []);
+
+  return (
+    <main className={styles.main}>
+      <img
+        src={innerWidth > 930 ? imageForLargeScreens : imageForSmallScreens}
+        alt="Una mujer usando un equipo de magnetoterapia"
+        className={styles.image}
+      />
+      <div className={styles.container}>
+        <h1 className={styles.title}>Alquiler de equipos de magnetoterapia en La Plata</h1>
+        <p className={styles.text}>
+          Acelerá tu recuperación desde casa con nuestros <strong>equipos de magnetoterapia</strong>
+          . Son fáciles de usar y te brindamos todas las instrucciones necesarias para que los
+          aproveches al máximo, sin complicaciones. Con más de 15 años de experiencia, te
+          acompañamos en cada paso de tu <strong>rehabilitación</strong>.
+        </p>
+        <Link to="/contacto" className={styles.button}>
+          Contacto
+        </Link>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import styles from "./App.module.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../components/Header/Header";
+import Home from "../pages/Home/Home";
 import Footer from "../components/Footer/Footer";
 
 const App = () => {
   return (
     <div className={styles.app}>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Navigate to="/" />} />
+      </Routes>
       <Footer />
     </div>
   );
