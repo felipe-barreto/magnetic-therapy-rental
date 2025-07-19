444649b440070eb69d9aecaa88c711a0eae690b3
Felipe Barreto <felipebarreto00000@gmail.com>
2025-07-19 20:01:59 -0300

    feat: create applicators view

---
+++ pages/Applicators/Applicators.tsx
@@ -0,0 +1,27 @@
+import styles from "./Applicators.module.css";
+import coils from "../../src/assets/magnetic-therapy-coils.png";
+import tunnel from "../../src/assets/tunnel-applicator.png";
+
+const Equipment = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Tipos de aplicadores de magnetoterapia</h1>
+      <div className={styles.container}>
+        <section>
+          <h2 className={styles.subtitle}>Bobinas planas</h2>
+          <img src={coils} alt="Bobinas planas" className={styles.image} />
+        </section>
+        <section>
+          <h2 className={styles.subtitle}>Túnel</h2>
+          <img src={tunnel} alt="Túnel" className={styles.image} />
+        </section>
+      </div>
+      <p className={styles.text}>
+        Contanos para qué zona del cuerpo necesitás tratamiento y te vamos a recomendar el aplicador
+        de magnetoterapia más adecuado para vos.
+      </p>
+    </main>
+  );
+};
+
+export default Equipment;
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styles from "./App.module.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "../components/Header/Header";
 import Home from "../pages/Home/Home";
+import Applicators from "../pages/Applicators/Applicators";
 import Footer from "../components/Footer/Footer";
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/aplicadores" element={<Applicators />} />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
